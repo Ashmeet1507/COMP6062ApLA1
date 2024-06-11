0872488cf5002e7f5e0c209c7150b7c52034e2da
Ashmeet1507 <a_passi@fanshaweonline.ca>
2024-06-11 19:53:50 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const studentName = "Ashmeet Passi"; 
+const studentNumber = "1159463"; 
+
+console.log(${AshmeetPassi} - ${1159463});
+
+const headerContent = document.querySelector('h1');
+headerContent.innerHTML = ${AshmeetPassi} - ${1159463};
+headerContent.classList.add('heading1');
